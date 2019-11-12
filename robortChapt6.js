c042729f00c329b84c757e5f405c2a1bacb08370
kagishokekana <57520281+kagishokekana@users.noreply.github.com>
2019-11-12 09:51:27 +0200

    Delete robortChapt6.js

--- robortChapt6.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("lefteye").style.backgroundColor =
-    "purple";
-document.getElementById("head").style.backgroundColor =
-    "yellow";
-document.getElementById("lefteye").style.backgroundColor =
-    "0";
